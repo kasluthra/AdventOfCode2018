a2a9f048fbbab8203d7d50bedc1c9190fd1b7cdf
Kasvi Luthra <F066018@westpacgroup.com>
2020-05-18 13:49:03 +1000

    This is a folder with the template files that can be used for each challenge

---
+++ Template/puzzle1.py
@@ -0,0 +1,7 @@
+#The purpose of this function is simply to read and understand the input file into a usable form and structure 
+def parse(lines):
+    return None
+
+#The purpose of this function is to solve the puzzle with the input structure 
+def solve(data):
+    return -1
