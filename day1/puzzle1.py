6d712e50b2570eedc916d9d9daeec6141c8f566a
Kasvi Luthra <F066018@westpacgroup.com>
2020-05-19 16:41:53 +1000

    Final code for part 1 of the first challenge is working

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -1,7 +1,20 @@
-#The purpose of this function is simply to read and understand the input file into a usable form and structure 
+data_file = open("input.txt", "r")
+
+#This function reads and understands the input file into a usable form and structure 
 def parse(lines):
-    return None
+    return lines
 
 #The purpose of this function is to solve the puzzle with the input structure 
 def solve(data):
+
+    total_fuel = 0
+
+    for x in range(len(data)):
+        fuel_x = (int(data[x])//3) - 2
+        total_fuel += fuel_x
+
+    print(total_fuel)
     return -1
+    
+data = parse(data_file.readlines())
+get_answer = solve(data)
